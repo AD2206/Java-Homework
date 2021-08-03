86596548db000a9bff9fa6c1db3818e4b7786e60
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2021-08-03 21:18:04 +0530

    Update divisibleBy7.java

--- src/divisibleBy7.java
+++ src/divisibleBy7.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class divisibleBy7 {
+import java.util.Scanner;
+
+public class divisibleBy7 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer a, b;
+
+        System.out.println("Enter Space Separated 2 Numbers");
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        if(a % 7 == 0 && b % 7 == 0){
+
+            System.out.println("Both are divisible by 7");
+        }
+
+        else{
+
+            System.out.println("Both are not divisible by 7");
+        }
+
+    }
+
 }
