2d798252d61737354a1cd901259f0ca078692971
Aryan <aryan.dubey2206@gmail.com>
2021-08-26 12:39:27 +0530

    Added Grade Calculation project

---
+++ src/gradeCalculation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class gradeCalculation {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter marks obtained in CS") ;
+
+        char grade = '.';
+        double marks = sc. nextDouble();
+
+        if(marks>=90) {
+            grade = 'A';
+        }
+            else if(marks>=70 && marks<=89) {
+            grade = 'B';
+
+        }
+            else if(marks>=50 && marks<=69) {
+            grade = 'C';
+        }
+            else if (marks<=50) {
+                grade ='D' ;
+            }
+
+            else{
+                System.out.println("Enter a valid mark");
+                System.exit(-1);
+        }
+
+            System.out.println("Grade Obtained by the Student =  " + grade);
+
+    }
+}
